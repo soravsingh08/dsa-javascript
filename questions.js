5d06db2bb28754678db20a44740d6bc20e1cabba
Sorav Singh <rahulrakwal3@gmail.com>
2025-12-06 16:13:58 +0530

    day01

---
+++ questions.js
@@ -0,0 +1,40 @@
+// Q1 WRITE FUN TO SEARCH AN ELEMENT IN AN ARRAY 
+// function searchElement(arr,elem){ 
+//     for (let i = 0;i<arr.length; i++){
+//         if(arr[i] === elem){
+//            return i ;
+//         }
+//     }
+//     return -1;    
+// }
+// let arr =  [3,5,6,8,1,52];
+// let result = searchElement(arr,8);
+// console.log(result)
+
+
+/// Q2 write a fun that return the no of negative number in array
+// let negativeNum = 0;
+// let arr = [1,-2,3,4,5,6,-4,-6,-8,-9];
+// function calculateNeg(){
+//     for(let i =0; i<arr.length; i++){
+//       if(arr[i] < 0){
+//          negativeNum ++
+//       } 
+//     }  
+// }
+// calculateNeg()
+// console.log(negativeNum)
+
+// Q3Write an function which return the largest number in an array.
+let lrgNum = -Infinity;
+let arr = [0,1,2,3,4,5,6,7,8,12,55,34,64,35]
+function largestNum(){
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i]>lrgNum){
+            lrgNum = arr[i];
+        }
+    }
+      return lrgNum;
+}
+largestNum()
+console.log(lrgNum)
