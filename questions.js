9936348bdae36beaf3516a06daf857999af7b668
Sorav Singh <rahulrakwal3@gmail.com>
2025-12-12 10:01:32 +0530

    Day 03

--- questions.js
+++ questions.js
@@ -26,15 +26,64 @@
 // console.log(negativeNum)
 
 // Q3Write an function which return the largest number in an array.
-let lrgNum = -Infinity;
-let arr = [0,1,2,3,4,5,6,7,8,12,55,34,64,35]
-function largestNum(){
+// let lrgNum = -Infinity;
+// let arr = [0,1,2,3,4,5,6,7,8,12,55,34,64,35]
+// function largestNum(){
+//     for(let i = 0; i<arr.length; i++){
+//         if(arr[i]>lrgNum){
+//             lrgNum = arr[i];
+//         }
+//     }
+//       return lrgNum;
+// }
+// largestNum()
+// console.log(lrgNum)
+
+
+//DAY 03 - SECOND LARGEST NUMBER
+
+// let lrgstNum = -Infinity
+// let secondLrgstNum = -Infinity
+// let arr =[4]
+
+// function secondLrgst(){
+//     if (arr.length < 2) {
+//      return null
+//     }
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i]> lrgstNum){
+//         secondLrgstNum = lrgstNum;
+//         lrgstNum = arr[i]
+//     }else if(secondLrgstNum > arr[i]){
+//        secondLrgstNum = lrgstNum;
+//     }
+//     }
+//     return secondLrgstNum
+// }
+
+// let sln = secondLrgst()
+// console.log(sln)
+
+
+
+
+let largestNum = -Infinity;
+let secondLrgst = -Infinity;
+let arr = [100,90,80];
+
+function secondLargestNum(){
     for(let i = 0; i<arr.length; i++){
-        if(arr[i]>lrgNum){
-            lrgNum = arr[i];
-        }
+       if(arr[i]>largestNum){
+        secondLrgst = largestNum;
+        largestNum = arr[i]
+       }
+
+       else if(arr[i] > secondLrgst){
+        secondLrgst = arr[i];
+       }
     }
-      return lrgNum;
+    return secondLrgst
 }
-largestNum()
-console.log(lrgNum)
+
+let sln = secondLargestNum()
+console.log(sln)
